85a34e39da7d9e133d23fffacd9d7b62919cb0c0
dmitri <dmitribazilov@gmail.com>
2022-11-25 12:27:35 +0300

    init dao

--- ifmo_web_lab_2/src/main/java/com/dmitri/ifmo_web_lab_2/model/HitCheck.java
+++ ifmo_web_lab_2/src/main/java/com/dmitri/ifmo_web_lab_2/model/HitCheck.java
@@ -5,6 +5,9 @@ import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.RequestScoped;
 import javax.inject.Inject;
 import java.io.Serializable;
+import com.dmitri.ifmo_web_lab_2.dao.HitCheckDao;
+import com.dmitri.ifmo_web_lab_2.dto.HitCheckDTO;
+import java.time.ZonedDateTime;
 
 @ManagedBean
 @RequestScoped
@@ -12,6 +15,9 @@ public class HitCheck implements Serializable {
 
     @ManagedProperty(value = "#{table}", name = "table")
     private Table table;
+
+    @ManagedProperty(value = "#{hitCheckDao}", name = "hitCheckDao")
+    private HitCheckDao hitCheckDao;
 
     private Double x;
     private Double y;
@@ -49,8 +55,28 @@ public class HitCheck implements Serializable {
         this.table = table;
     }
 
+    public HitCheckDao getHitCheckDao() {
+        return hitCheckDao;
+    }
+
+    public void setHitCheckDao(HitCheckDao hitCheckDao) {
+        this.hitCheckDao = hitCheckDao;
+    }
+
     public void save() {
-        System.out.println(table);
+        HitCheckDTO dto = new HitCheckDTO();
+        dto.setX(x);
+        dto.setY(y);
+        dto.setR(r);
+        dto.setHitDate(ZonedDateTime.now());
+        dto.setExecuteTime(2L);
+        dto.setResult(false);
+        try {
+            hitCheckDao.createHitCheck(dto);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        System.out.println(dto);
         table.getHits().add(this);
     }
 }
