2115d23ceda7d59fb4f27a6e041d5217d99a4cc7
dmitri <dmitribazilov@gmail.com>
2022-12-22 15:34:36 -0800

    disable ice

--- ifmo_web_lab_2/src/main/webapp/js/validator.js
+++ ifmo_web_lab_2/src/main/webapp/js/validator.js
@@ -1,36 +1,4 @@
-function validate_values(x, y, r) {
-	result = "";
-	result += validate_x(x)	+ validate_y(y) + validate_r(r);
-	return result;
-}
-
-function validate_x(x) {
-	if (x.length === 0) {
-		return "Choose X please. <br>";
-	}
-    console.log(x);
-    let flag = false;
-    x.forEach(function (element) {
-        console.log(element.value);
-        if (!parseFloat(element.value) && element.value !== "0") flag = true;
-    });
-    if (flag) return "Choose correct X. <br>";
-	return "";
-}
-
-function validate_y(y) {
-	let newY = y.value.replace(",", ".");
-	if (isNaN(newY) || isNaN(parseFloat(newY))) {
-		return "Y must be a number <br>";
-	} else if (parseFloat(newY) > 3 || parseFloat(newY) < -5) {
-		return "Y must be in [-5; 3] <br>";
-	} else {
-		return "";
-	}
-}
-
 function checkY() {
-    console.log("EEEEEEEE");
     field = $('[name="formSend:y_value"]');
     string_field = field.val();
     string_field = string_field.replace(',', '.');
